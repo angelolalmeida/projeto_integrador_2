a9de0f698354870c0ad5d6ba65b49d9ee739f6ec
Denis Souza <souzadf09@gmail.com>
2023-10-21 22:12:59 -0300

    Development in coletas, was install apps coleta

--- coletas/models.py
+++ coletas/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 
+from django.db import models
 
 class Coleta(models.Model):
     DATA_CHOICES = (
--- coletas/views.py
+++ coletas/views.py
@@ -1,9 +1,26 @@
+# from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+
+# # Create your views here.
+
+# @login_required
+# def index(request):
+
+#     # Page from the theme
+#     return render(request, 'pages/index.html')
+
+
+# def coleta_list(request):
+#     coletas = Coleta.objects.all()
+#     return render(request, 'coleta_list.html', {'coletas': coletas})
+
 from django.shortcuts import render
+from .models import Coleta
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required
 def index(request):
 
     # Page from the theme
