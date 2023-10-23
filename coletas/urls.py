549c1e76a07aaa7c8c691cf9a56944b1c8a4485f
Denis Souza <souzadf09@gmail.com>
2023-10-23 20:59:04 -0300

    correção de urls

--- coletas/urls.py
+++ coletas/urls.py
@@ -12,8 +12,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('coletas/', views.coleta_list, name='coleta_list'),
-    path('coletas/', views.coletas_page, name='coletas'),
+    path('', views.coleta_list, name='coleta_list'),
+    #path('coletas/', views.coleta_list, name='coleta_list'),
+    #path('coletas/', views.coletas_page, name='coletas'),
     # Adicione outras URLs conforme necessário
 ]
