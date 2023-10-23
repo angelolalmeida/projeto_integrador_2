a3f696008a04902217366255289e7403c699ad93
Denis Souza <souzadf09@gmail.com>
2023-10-23 19:08:20 -0300

    coletas

--- coletas/urls.py
+++ coletas/urls.py
@@ -1,8 +1,19 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('coletas/', views.coleta_list, name='coleta_list'),
+#     path('coletas.html/', views.coletas_page, name='coletas.html'),
+#     # Adicione outras URLs conforme necessário
+# ]
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('coletas/', views.coleta_list, name='coleta_list'),
+    path('coletas/', views.coletas_page, name='coletas'),
     # Adicione outras URLs conforme necessário
 ]
--- coletas/views.py
+++ coletas/views.py
@@ -1,3 +1,20 @@
+# from django.shortcuts import render
+# from .models import Coleta
+# from django.contrib.auth.decorators import login_required
+
+# # Create your views here.
+
+# @login_required
+# def index(request):
+
+#     # Page from the theme
+#     return render(request, 'pages/index.html')
+
+# def coleta_list(request):
+#      coletas = Coleta.objects.all()
+#      return render(request, 'coleta_list.html', {'coletas': coletas})
+
+
 from django.shortcuts import render
 from .models import Coleta
 from django.contrib.auth.decorators import login_required
@@ -6,11 +23,12 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-
-    # Page from the theme
+    # Página do tema
     return render(request, 'pages/index.html')
-
 
 def coleta_list(request):
     coletas = Coleta.objects.all()
     return render(request, 'coleta_list.html', {'coletas': coletas})
+
+def coletas_page(request):
+    return render(request, 'coletas.html')
