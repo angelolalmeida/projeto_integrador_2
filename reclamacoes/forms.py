0c61b509c83468fe50cb18a27fbe156a678a90b8
vitoroliveira <vitor.fellipe@hotmail.com>
2023-10-25 20:16:01 -0300

    codigo antes de apagar comentarios

--- reclamacoes/forms.py
+++ reclamacoes/forms.py
@@ -4,26 +4,27 @@ from .models import Reclamacoes
 
 def validate_cpf(value):
     if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números.')
+        raise ValidationError(' Campo CPF deve conter apenas números.')
     if len(value) != 11:
-        raise ValidationError('CPF deve conter exatamente 11 dígitos.')
+        raise ValidationError(' Campo CPF deve conter exatamente 11 dígitos.')
 
 class ReclamacoesForm(forms.ModelForm):
     cpf = forms.CharField(label='CPF',validators=[validate_cpf], widget=forms.TextInput(attrs={'id': 'id_cpf'}), required=False)
-    acompanhareclamacao = forms.BooleanField(label='PARA ACOMPANHAR SUA RECLAMAÇÃO SELECIONE A OPÇÃO E PRENCHA SEU CPF',required=False, widget=forms.CheckboxInput(attrs={'id': 'id_acompanhareclamacao'}))
+    acompanhar_reclamacao = forms.BooleanField(label='DESEJO ACOMPANHAR MINHA RECLAMAÇÃO.',required=False, widget=forms.CheckboxInput(attrs={'id': 'id_acompanhar_reclamacao'}))
 
     class Meta:
         model = Reclamacoes
         fields = ['nome','tipo_reclamacao', 'observacao',
                   'cep', 'rua', 'numero_casa', 'complemento',
-                  'referencia', 'telefone', 'acompanhareclamacao','cpf',]
-        labels = {'acompanhareclamacao':'OPCIONAL','telefone': 'TELEFONE','complemento': 'COMPLEMENTO','numero_casa': 'NÚMERO CASA','rua': 'RUA','cep': 'CEP','nome': 'NOME','cpf': 'CPF','observacao': 'DETALHES DA RECLAMAÇÃO','tipo_reclamacao': 'TIPO DE RECLAMAÇÃO', 'numero_casa': 'NÚMERO DA CASA','referencia':'PONTO DE REFERÊNCIA' }
-        help_texts = {}
+                  'referencia', 'telefone', 'acompanhar_reclamacao','cpf',]
+        labels = {'acompanhar_reclamacao':'OPCIONAL','telefone': 'TELEFONE','complemento': 'COMPLEMENTO','numero_casa': 'NÚMERO CASA','rua': 'RUA','cep': 'CEP','nome': 'NOME','cpf': 'CPF','observacao': 'DETALHES DA RECLAMAÇÃO','tipo_reclamacao': 'TIPO DE RECLAMAÇÃO', 'numero_casa': 'NÚMERO DA CASA','referencia':'PONTO DE REFERÊNCIA' }
+        help_texts = {'acompanhar_reclamacao': 'Se deseja acompanhar a sua reclamação, selecione a opção abaixo e insira seu CPF.'
+}
 
     def clean(self):
         cleaned_data = super().clean()
         cpf = cleaned_data.get('cpf')
-        acompanhareclamacao = cleaned_data.get('acompanhareclamacao')
+        acompanhar_reclamacao = cleaned_data.get('acompanhar_reclamacao')
 
-        if acompanhareclamacao and not cpf:
-            self.add_error('cpf', 'CPF é obrigatório quando a opção CPF Opcional está selecionada.')
+        if acompanhar_reclamacao and not cpf:
+            self.add_error('cpf', 'Para acompanhar sua reclamação o campo CPF é obrigatório')
--- reclamacoes/models.py
+++ reclamacoes/models.py
@@ -10,18 +10,19 @@ STATUS_CHOICES = [
 ]
 
 TIPO_RECLAMACAO_CHOICES = [
-    ('nao_recebi', 'Não recebi o saco verde'),
-    ('caminhao_nao_passou', 'Caminhão não passou'),
-    ('saco_rasgado', 'Saco rasgado'),
-    ('sugestao', 'Sugestão'),
-    ('outros', 'Outros'),
+    ('nao_recebi_saco', 'NÃO RECEBI O SACO VERDE'),
+    ('caminhao_nao_passou', 'CAMINHÃO NÃO PASSOU'),
+    ('saco_rasgado', 'SACO VERDE RASGADO'),
+    ('saco_insuficiente', 'SACO VERDE INSUFICIENTE'),
+    ('sugestao', 'SUGESTÃO'),
+    ('outros', 'OUTRO'),
 ]
 
 class Reclamacoes(models.Model):
     nome = models.CharField(max_length=100, null=True)
     cpf = CPFField('cpf')
-    acompanhareclamacao = models.BooleanField(default=False)
-    tipo_reclamacao = models.CharField(max_length=20, choices=TIPO_RECLAMACAO_CHOICES, default='outros')
+    acompanhar_reclamacao = models.BooleanField(default=False)
+    tipo_reclamacao = models.CharField(max_length=20, choices=TIPO_RECLAMACAO_CHOICES)
     observacao = models.TextField(max_length=500)
     cep = models.CharField(max_length=100, null=True)
     rua = models.CharField(max_length=200, blank=True)
@@ -31,7 +32,7 @@ class Reclamacoes(models.Model):
     referencia = models.CharField(max_length=100, blank=True, null=True)
     telefone = models.CharField(max_length=20, blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pendente', editable=False)
-    historico = models.TextField(blank=True, null=True, editable=False)
+    historico = models.TextField(blank=True, null=True, editable=False,default='Aguardando resposta')
     data_criacao = models.DateTimeField(default=timezone.now, editable=False)
 
     def save(self, *args, **kwargs):
