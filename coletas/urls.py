7db765d0305c9b9f263f6f9c83ab8fd432070068
Angelo Almeida <angelolalmeida@yahoo.com.br>
2023-11-02 19:34:43 -0300

    correção das migrations

--- coletas/urls.py
+++ coletas/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.coleta_list, name='coletas_lista'),    
+    path('', views.coleta_list, name='coletas_lista'),
 ]
-
--- coletas/views.py
+++ coletas/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import Coleta
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
 @login_required
 def index(request):
